5cf36e21d8185c81ca018c746665828f70d72848
Wojciech Reise <reisewojtus@gmail.com>
2020-02-07 12:14:19 +0100

    Adapt the parameter name change to the effective parameter

--- gtda/utils/validation.py
+++ gtda/utils/validation.py
@@ -20,7 +20,7 @@ available_metrics = {
     'persistent_image': [('order', numbers.Number, (1, np.inf)),
                          ('n_values', int, (1, np.inf)),
                          ('sigma', numbers.Number, (0., np.inf)),
-                         ('_weight_function', types.FunctionType,
+                         ('effective_weight_function_', types.FunctionType,
                           None)]}
 
 available_metric_params = list(set(
